6e4c06ae6f747d417384d01671b42de9b141a34e
Moritz Schmidt <schmidtmoritz404@gmail.com>
2026-09-07 08:27:36 +0200

    postgres: split database and user definition

--- ansible_collections/giftpilz0/container/roles/postgresql/templates/postgresql-pod/config/init-databases.sql
+++ ansible_collections/giftpilz0/container/roles/postgresql/templates/postgresql-pod/config/init-databases.sql
@@ -1,14 +1,16 @@
-{% for database in postgresql_databases %}
+{% for user in postgresql_users | default([]) %}
 DO $$
 BEGIN
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '{{ database.user | replace("'", "''") }}') THEN
-    CREATE ROLE "{{ database.user | replace('"', '""') }}" LOGIN PASSWORD '{{ database.password | replace("'", "''") }}';
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '{{ user.name | replace("'", "''") }}') THEN
+    CREATE ROLE "{{ user.name | replace('"', '""') }}" LOGIN PASSWORD '{{ user.password | replace("'", "''") }}';
   END IF;
 END
 $$;
+{% endfor %}
 
-SELECT 'CREATE DATABASE "{{ database.name | replace('"', '""') }}" OWNER "{{ database.user | replace('"', '""') }}"'
+{% for database in postgresql_databases %}
+SELECT 'CREATE DATABASE "{{ database.name | replace('"', '""') }}" OWNER "{{ database.owner | replace('"', '""') }}"'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '{{ database.name | replace("'", "''") }}')\gexec
 
-ALTER DATABASE "{{ database.name | replace('"', '""') }}" OWNER TO "{{ database.user | replace('"', '""') }}";
+ALTER DATABASE "{{ database.name | replace('"', '""') }}" OWNER TO "{{ database.owner | replace('"', '""') }}";
 {% endfor %}
